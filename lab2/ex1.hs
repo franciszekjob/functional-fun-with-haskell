f473c157036877c4058d42d0b532b6aed4e8d3c4
Franciszek Job <jellybean@onet.eu>
2023-11-16 15:22:01 +0100

    currying, partially applied functions

---
+++ lab2/ex1.hs
@@ -0,0 +1,25 @@
+myFun x = 2 * x
+
+add2T :: Num a => (a, a) -> a
+add2T (x,y) = x + y
+
+add2C :: Num a => a -> a -> a
+add2C x y = x + y
+
+add3T :: Num a => (a, a, a) -> a
+add3T (x,y,z) = x + y + z
+
+add3C :: Num a => a -> a -> a -> a
+add3C x y z = x + y + z
+
+fiveToPower_ :: Integer -> Integer
+fiveToPower_ = (5 ^)
+
+_ToPower5 :: Num a => a -> a
+_ToPower5 = (^ 5)
+
+subtrNFrom5 :: Num a => a -> a
+subtrNFrom5 n = 5 - n
+
+subtr5From_ :: Num a => a -> a
+subtr5From_ = subtract 5
