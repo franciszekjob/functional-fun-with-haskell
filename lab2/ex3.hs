95edce3d9f254adab2b088e2ff4992fbc40f7ed8
Franciszek Job <jellybean@onet.eu>
2023-11-16 17:13:32 +0100

    functions with accumulator

--- lab2/ex3.hs
+++ lab2/ex3.hs
@@ -30,3 +30,13 @@ squareAll (x:arr) = (x^2) : squareAll arr
 selectEven :: Integral t => [t] -> [t] -- selectEven [1,2,3] = [2]
 selectEven [] = []
 selectEven (x:arr) = if x `mod` 2 == 0 then x : selectEven arr else selectEven arr
+
+prod'2 :: Num a => [a] -> a
+prod'2 = loop 1
+    where loop acc []      = acc
+          loop acc (x:arr) = loop (x * acc) arr
+
+length'2 :: Num a => [a] -> a
+length'2 = loop 0
+    where loop acc []      = acc
+          loop acc (x:arr) = loop (1 + acc) arr
