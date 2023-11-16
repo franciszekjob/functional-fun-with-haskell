087ffd6a5da1677bd21fd62b7dc8a05b2c7f24b9
Franciszek Job <jellybean@onet.eu>
2023-11-16 16:52:36 +0100

    recursion

---
+++ lab2/ex3.hs
@@ -0,0 +1,32 @@
+fib :: (Num a, Eq a) => a -> a
+fib n =
+    if n == 0 || n == 1 then 1
+    else fib (n-2) + fib (n - 1)
+
+sum' :: Num a => [a] -> a
+sum' []   = 0
+sum' (x:xs) = x + sum' xs
+
+prod' :: Num a => [a] -> a -- prod' [1,2,3] = 6
+prod' [] = 1
+prod' (x:arr) = x * (prod' arr)
+
+length' :: [a] -> Int -- length' [1,1,1,1] = 4
+length' [] = 0
+length' (x:arr) = 1 + length' arr
+
+elem' :: Eq a => a -> [a] -> Bool -- elem' 3 [1,2,3] = True
+elem' _ [] = False
+elem' e (x:arr) = e == x || elem' e arr
+
+doubleAll :: Num t => [t] -> [t] -- doubleAll [1,2] = [2,4]
+doubleAll [] = []
+doubleAll (x:arr) = (x*2) : doubleAll arr
+
+squareAll :: Num t => [t] -> [t] -- squareAll [2,3] = [4,9]
+squareAll [] = []
+squareAll (x:arr) = (x^2) : squareAll arr
+
+selectEven :: Integral t => [t] -> [t] -- selectEven [1,2,3] = [2]
+selectEven [] = []
+selectEven (x:arr) = if x `mod` 2 == 0 then x : selectEven arr else selectEven arr
